c6e87f580f4ad3f15745584fcd162b646db19511
AmalaAnanth381 <45572910+AmalaAnanth381@users.noreply.github.com>
2020-08-14 23:40:27 +0530

    Create Python_Practice_Programs_10.py

---
+++ Python_Practice_Programs_10.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+arg=[3,2,3,4]
+print(arg)
+arg[0] += arg[1]
+print ("\n Addition of two numbers is", arg[0])
+arg[0] -= arg[1]
+print ("\n Subtraction of two numbers is", arg[0])
+arg[0] *= arg[1] 
+print ("\n Multiplication of two numbers is", arg[0])
+arg[0] /= arg[1] 
+print ("\n Division of two numbers is", arg[0])
+arg[0] %= arg[1]
+print ("\n modulus of two numbers is", arg[0])
+arg[0] **= arg[1]
+print ("\n Exponentiation of two numbers is", arg[0])
+arg[0] //= arg[1]
+print ("\n Floor division of two numbers is", arg[0])
