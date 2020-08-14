0ebd7d2b944610a352e0efaa88fb15797de5663d
AmalaAnanth381 <45572910+AmalaAnanth381@users.noreply.github.com>
2020-08-14 23:38:24 +0530

    Create Python_Practice_Programs_09.py

---
+++ Python_Practice_Programs_09.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+arg=[3,2,3,4]
+a = complex(arg[0], arg[1])
+b = complex(arg[2], arg[3])
+c = a + b
+print ("\n Addition of two numbers is", c)
+c = a- b
+print ("\n Subtraction of two numbers is", c)
+c = a * b 
+print ("\n Multiplication of two numbers is", c)
+c = a / b 
+print ("\n Division of two numbers is", c)
