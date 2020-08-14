87bc610d76a5fba6e5a669a9f62b54d7e2ea9abf
AmalaAnanth381 <45572910+AmalaAnanth381@users.noreply.github.com>
2020-08-14 23:30:34 +0530

    Create Python_Practice_Programs_05.py

---
+++ Python_Practice_Programs_05.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import sys
+arg1=sys.argv[0]
+arg2=sys.argv[1]
+arg3=sys.argv[2]
+print("First number is :", arg1 )
+print ("Second number is :",arg2)
+print ("Third number is : ", arg3)
+print ("The biggest of three numbers is :", max(arg1, arg2, arg3))
