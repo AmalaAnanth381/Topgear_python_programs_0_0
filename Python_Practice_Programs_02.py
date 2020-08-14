8c088562a6826e7181ad1e9001944db1db649c25
AmalaAnanth381 <45572910+AmalaAnanth381@users.noreply.github.com>
2020-08-14 23:26:18 +0530

    Create Python_Practice_Programs_02.py

---
+++ Python_Practice_Programs_02.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+a, b, c = 20, 10, 30
+if (a > b and a > c):  print ("Biggest of three numbers is",a)
+elif  (b > a and b> c):  print ("Biggest of three numbers is", b)
+else :  print ("Biggest of three numbers is", c)
